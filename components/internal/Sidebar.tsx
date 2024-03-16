24b1e0d082bdb1cc56d70c362852c253751975d4
Ayush Kaithwas <ayush.91011@gmail.com>
2024-03-16 16:06:50 +0530

    fix: layers dropdown scrollable

--- components/internal/Sidebar.tsx
+++ components/internal/Sidebar.tsx
@@ -10,6 +10,7 @@ import {
 } from "../ui/dropdown-menu";
 import { input, layers, Layer as L } from "@/packages/tf";
 import { useCallback } from "react";
+import { ScrollArea, ScrollBar } from "../ui/scroll-area";
 
 let id = 1;
 const getId = () => `${id++}`;
@@ -53,27 +54,30 @@ export function Sidebar({
         <Play fill="black" />
         <h2>Input</h2>
       </Button>
-      <DropdownMenu>
+      <DropdownMenu modal={false}>
         <DropdownMenuTrigger className="bg-white flex flex-col hover:bg-accent hover:text-accent-foreground items-center border rounded-md p-2 justify-around h-[5rem] w-[5rem]">
           <Layers3 />
           <h2>Layer</h2>
         </DropdownMenuTrigger>
-        <DropdownMenuContent className="flex flex-col pl-2 ml-5">
+        <DropdownMenuContent className="flex flex-col pl-2 ml-5 max-h-[25rem] max-w-xl">
           <DropdownMenuLabel>Layers</DropdownMenuLabel>
           <DropdownMenuSeparator />
-          {layers.map((layer) => {
-            const newLayer = layer();
-            return (
-              <DropdownMenuItem
-                key={layer.name}
-                onClick={() => {
-                  createLayerNode(newLayer);
-                }}
-              >
-                {newLayer.name}
-              </DropdownMenuItem>
-            );
-          })}
+          <ScrollArea className="w-xl h-[25rem] rounded-md">
+            {layers.map((layer) => {
+              const newLayer = layer();
+              return (
+                <DropdownMenuItem
+                  key={layer.name}
+                  onClick={() => {
+                    createLayerNode(newLayer);
+                  }}
+                >
+                  {newLayer.name}
+                </DropdownMenuItem>
+              );
+            })}
+            <ScrollBar orientation="vertical" />
+          </ScrollArea>
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
