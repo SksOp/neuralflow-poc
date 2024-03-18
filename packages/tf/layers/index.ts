7a93535a952f2a50b673f9a0af218d9d42d8aeb7
Ayush Kaithwas <ayush.91011@gmail.com>
2024-03-18 21:00:40 +0530

    fix: sorting alphabetically and removed unused assets

--- packages/tf/layers/index.ts
+++ packages/tf/layers/index.ts
@@ -102,4 +102,16 @@ export const layers = [
   unit_normalization,
   time_distributed,
   thresholded_relu,
-].sort();
+].sort((a, b) => {
+  const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  // names must be equal
+  return 0;
+});
