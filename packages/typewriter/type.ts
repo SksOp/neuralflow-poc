5253927ce19bf514f047829a875824295fb86b00
SksOp <shubhaman47@gmail.com>
2024-03-21 13:15:50 +0530

    remove: unwanted codes

--- packages/typewriter/type.ts
+++ packages/typewriter/type.ts
@@ -4,28 +4,12 @@ export interface py {
   toCodeString(): string;
 }
 
-export type WithStaticOf<T> = {
-  new (...args: any[]): T; // Constructor signature
-  of: (...args: any[]) => T;
-};
+// export type WithStaticOf<T> = {
+//   new (...args: any[]): T; // Constructor signature
+//   of: (...args: any[]) => T;
+// };
 
-// export enum supported_types {
-//   int,
-//   str,
-//   float,
-//   complex,
-//   bool,
-//   list,
-//   tuple,
-//   range,
-//   dict,
-//   frozenset,
-//   set,
-//   bytes,
-//   bytearray,
-//   memoryview,
-//   noneType,
-// }
+
 
 export enum supported_types {
   int = "int",
